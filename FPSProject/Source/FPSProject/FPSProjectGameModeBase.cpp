e4f1ba19496ff592e326f08d6265427615c682c4
Rodrigodoo <rodrigo.do.o.barbosa@gmail.com>
2018-12-05 14:23:10 +0000

    Created a Game Mode override: - Displays a Debug Message - Displays a Log Message

--- FPSProject/Source/FPSProject/FPSProjectGameModeBase.cpp
+++ FPSProject/Source/FPSProject/FPSProjectGameModeBase.cpp
@@ -1,4 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "FPSProjectGameModeBase.h"
+#include "Engine.h"
 
+void AFPSProjectGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+
+	//Start Game Message
+#if WITH_EDITOR
+	//Debug Message
+	if(GEngine)
+	{
+		//Display Debug message for five seconds
+		//the -1 "Key2 value ( first argument) indicates that we will never need to update or refresh this message
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is the FPS GameMode!"));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Game Started!"));
+#endif
+}
